b75590efe116872b7658d80fe7cfc1c76d4b31c7
Ishwarmahajan <77274693+Ishwarmahajan@users.noreply.github.com>
2021-03-28 08:53:28 +0530

    Add files via upload

---
+++ paperball.js
@@ -0,0 +1,28 @@
+class Paperball{
+    constructor(x, y, radius){
+        var options ={
+            isStatic : false,
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+        }
+
+        this.body = Bodies.circle(x, y, radius, options)
+        this.radius = radius
+        World.add(world, this.body)
+
+       // Matter.Body.setMass(this.body, this.body.mass * 5)
+
+    }
+
+    display(){
+        var pos = this.body.position
+        
+        push()
+        //translate(pos.x, pos.y)
+        ellipseMode(CENTER)
+        fill("white")
+        ellipse(pos.x, pos.y, this.radius, this.radius)
+        pop()
+    }
+}
